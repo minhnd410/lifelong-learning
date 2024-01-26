3af8e5a53cdf72af2a5f61dffec174332d8a7261
Minh Nguyen <minhnd410@gmail.com>
2024-01-26 23:25:17 +0700

    move common

--- dotnet/Herond.Common/FileService/IFileStorageService.cs
+++
@@ -1,8 +0,0 @@
-namespace Herond.Common.FileService;
-
-public interface IFileStorageService
-{
-    Task UploadFileAsync(string fileName, Stream content);
-    Task<Stream> GetFileAsync(string fileName);
-    Task DeleteFileAsync(string fileName);
-}
--- dotnet/Herond.Common/README.md
+++
@@ -1,8 +0,0 @@
-## ApiKeyValidator
-
-```csharp
-services.AddSingleton<IApiKeyValidator>(_ => new ApiKeyValidator(_apiKey));
-
-var apiV1 = app.MapGet( "/some-endpoint" )
-    .AddEndpointFilter<ApiKeyFilter>();
-```
